3349860807ac8be09c8fc9e616bdfd4d06ca7279
Langley <plangley@us-bottomline.root.bottomline.com>
2020-10-21 16:56:26 -0400

    updated dictionay file

--- Dictionaries.py
+++ Dictionaries.py
@@ -10,3 +10,15 @@ print(friend_ages["Rolf"])
 #dictionary[key]=value
 friend_ages["Bob"] = 70
 print(friend_ages)
+
+#a tuple of dictionaries
+#use open parens for tupple - curly brackets for dictionary
+friends = ( 
+    {"name": "Rolf Smith", "age": 24},
+    {"name": "Adam Wool", "age": 30},
+    {"name": "Anne Pun", "age": 27}
+)
+#to get a friends name, in this case fiend #0's name
+print(friends[0]["name"])
+#get the age for Anne
+print(friends[2]["age"])
