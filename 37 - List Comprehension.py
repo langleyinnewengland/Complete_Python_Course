828acbed4e7a77136a85a273e75b686fc53756e2
Peter Langley <plangley@bottomline.com>
2020-10-27 17:17:56 -0400

    changes

--- 37 - List Comprehension.py
+++ 37 - List Comprehension.py
@@ -16,5 +16,20 @@ numbers = [0, 1, 2, 3, 4]  # list(range(5)) is better
 doubled_numbers = [num * 2 for num in numbers]
 # [num * 2 for num in range(5)] would be even better.
 
-
+#print the list in lowercase
 names = ["Rolf", "Bob", "Jen"]
+lower =[name.lower() for name in names] #.lower() method puts case to lower
+print(lower)
+
+#check to see if the user entry matches what is in the list.  But convert user entry and list in memory to 
+#the same case (lower)
+
+friend = input("Enter your friend name: ")
+friends = ["Rolf", "Bob", "Jen", "Charlie", "Anne"]
+friends_lower = [name.lower() for name in friends]
+
+if friend.lower() in friends_lower:
+    print(f"I know {friend}!")
+    #use the title method to print friend in title case
+    print(f"I know {friend.title()}!")
+    
