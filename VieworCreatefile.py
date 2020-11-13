2e123a1ab3d6202bd927dc438c5e9c12f046a302
Peter Langley <plangley@bottomline.com>
2020-11-13 18:51:00 -0500

    updates to add to menu.  This might be broken

--- VieworCreatefile.py
+++ VieworCreatefile.py
@@ -2,16 +2,20 @@ import yaml
 a_yaml_file = open("11_9_swagger.yaml")
 #asssigns the contact to yamlfile
 yamlfile = yaml.load(a_yaml_file, Loader= yaml.FullLoader)
+description = yamlfile['info']['description']
+tags = yamlfile['tags']
+specifictags = yamlfile['tags'][0]['name']
 
 def choice():
-    menuchoice = input("Would you like to (o) open or (c) create: ")
+    menuchoice = input("Would you like to (o) open or (c) create or a (append): ")
     if (menuchoice == 'o'):
         openfile()
     elif (menuchoice == 'c'):
         namefile()
-
+    elif (menuchoice == 'a'):
+        appendfile()
 def namefile():
-    description = yamlfile['info']['description']
+
     namefile = input ("What is the name of the file to create?:")
     #open in "w" mode creates new file, "a" is append
     with open(namefile, "w") as myfile:
@@ -24,6 +28,19 @@ def openfile():
     with open(whatfile, 'r') as readfile:
         print(readfile.read())
         choice()
+
+def appendfile():
+    nameappendfile = input("What is the name of the file to append?:")
+    whattoappend = input("What do you want to append? (d) description (t) tags or (s) specifc tags?: ")
+    if (whattoappend) == 'd':
+        with open(nameappendfile, "a") as myfile:
+            myfile.write(description)
+            myfile.close()
+    elif (whattoappend) == 't':
+        with open(nameappendfile, "a") as myfile:
+            myfile.write(tags)
+            myfile.close()
+            choice()
 
 
 choice()
